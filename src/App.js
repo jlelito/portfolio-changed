10f8274a373b9b38621b4a08fcf3faffb1788b00
jlelito <joshualelito@gmail.com>
2020-01-15 16:11:04 -0500

    added more profile data

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Experience from './Components/Experience';
 import Skills from './Components/Skills';
 import Education from './Components/Education';
 import Interests from './Components/Interests';
+import Projects from './Components/Projects';
 
 import profileData from './profileData.json';
 
@@ -31,12 +32,15 @@ class App extends Component {
           <hr className="m-0" />
           <Experience experience={this.state.experience} />
           <hr className="m-0" />
-          <Skills skills={this.state.skills} />
+          <Projects projects={this.state.projects} />
           <hr className="m-0" />
           <Education education={this.state.education}/>
           <hr className="m-0" />
+          <Skills skills={this.state.skills} />
+          <hr className="m-0" />
           <Interests interests={this.state.interests} />
-
+          <hr className="m-0" />
+          
         </div>
       </div>
     );
