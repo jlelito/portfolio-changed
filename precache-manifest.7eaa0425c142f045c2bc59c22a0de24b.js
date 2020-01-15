77bda05a5cb02291583cf65186494932d97d6f7c
jlelito <joshualelito@gmail.com>
2020-01-15 16:12:26 -0500

    Updates

---
+++ precache-manifest.7eaa0425c142f045c2bc59c22a0de24b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "140504339d1e766a892992e847dc3007",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "df1e852d22986316b062",
+    "url": "/portfolio/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "7757599d4a86c62a2f07",
+    "url": "/portfolio/static/css/main.6717f28a.chunk.css"
+  },
+  {
+    "revision": "df1e852d22986316b062",
+    "url": "/portfolio/static/js/2.7e493f3d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.7e493f3d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7757599d4a86c62a2f07",
+    "url": "/portfolio/static/js/main.3175dcab.chunk.js"
+  },
+  {
+    "revision": "bce83eb0e29e7b124e91",
+    "url": "/portfolio/static/js/runtime-main.e497cdd9.js"
+  },
+  {
+    "revision": "2506ae711ef4617c430cd5ff803eb509",
+    "url": "/portfolio/static/media/loan.2506ae71.jpg"
+  },
+  {
+    "revision": "92402475f93be795b77177c2c036c5e1",
+    "url": "/portfolio/static/media/me.92402475.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.8858e872cece61ab836d38c7315aa823.js"
+  "/portfolio/precache-manifest.7eaa0425c142f045c2bc59c22a0de24b.js"
 );
 
 self.addEventListener('message', (event) => {
