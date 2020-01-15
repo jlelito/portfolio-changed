47d27e5e279d63b6c94e16ecbaac5220d610bba2
jlelito <joshualelito@gmail.com>
2020-01-15 17:04:10 -0500

    Adding custom project images for cards

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import image from '../Assets/loan.jpg'
 import BootstrapCard from './Cards/CardUI.jsx';
+import event from '../Assets/loan.jpg'
 
 
 
@@ -12,6 +12,8 @@ class Projects extends Component {
   }
 
   render() {
+
+
     return(
       <section className="resume-section p-3 p-lg-5 d-flex align-items-center" id="projects">
         <div className="w-100">
@@ -25,7 +27,7 @@ class Projects extends Component {
             this.projects.map((data, index) => (
               
               <BootstrapCard 
-                imgsrc={image} 
+                imgsrc={require(`../Assets/${data.image}.jpg`)} 
                 title={data.projectName} 
                 cardtext={data.projectDetail}
                 link={data.link}
